92d5445feed75df59e583dafa1cbb9406458e619
vladimir.klapan <vladimir.klapan@gmail.com>
2015-10-11 21:18:35 +0300

    20151011 startFragmentsViewEdit

---
+++ PKUDZ06/app/src/main/java/com/example/klapan/pkudz06/FragmentItemView.java
@@ -0,0 +1,7 @@
+package com.example.klapan.pkudz06;
+
+/**
+ * Created by klapan on 10/10/15.
+ */
+public class FragmentItemView {
+}
---
+++ PKUDZ06/app/src/main/java/com/example/klapan/pkudz06/MainActivityFragments.java
@@ -0,0 +1,126 @@
+package com.example.klapan.pkudz06;
+
+
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivityFragments extends ActionBarActivity {
+
+    private static final int REQUEST_CODE_NEW_ADD = 1;
+    private ArrayList<AddItems> items;
+    private  ListView listItems;
+    private MyArrayAdapter myAdapter;
+    private AddSQLiteOpenHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = new AddSQLiteOpenHelper(this);
+
+        items = db.getAllAddItemsFromSql();
+
+        listItems = (ListView) findViewById(R.id.listViewItems);
+
+        myAdapter = new MyArrayAdapter(this, items);
+        listItems.setAdapter(myAdapter);
+
+        AdapterView.OnItemClickListener onClickListenerListItems = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                FragmentItemView fragment = new FragmentItemView();
+                fragmentTransaction.add(R.id.fragmentView, fragment);
+                fragmentTransaction.commit();
+            }
+        };
+
+        listItems.setOnItemClickListener(onClickListenerListItems);
+
+        AdapterView.OnItemLongClickListener onLongClickListenerListItems = new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick (AdapterView<?> parent, View view, int position, long id) {
+                Log.e("my_log", "long click");
+                return true;
+            }
+        };
+
+        listItems.setOnItemLongClickListener(onLongClickListenerListItems);
+
+
+    }
+
+    public void showSetDistrictDialog() {
+        DialogFragment dialog = new setDistrictDialog();
+        dialog.show(getFragmentManager(), "districts");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_new) {
+            Intent intent = new Intent(this, ActivityForm1.class);
+            startActivityForResult(intent, REQUEST_CODE_NEW_ADD);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent intentItem){
+
+        if (requestCode == REQUEST_CODE_NEW_ADD && resultCode == RESULT_OK){
+            Bitmap img = null;
+            try {
+                img = intentItem.getParcelableExtra("bitmap_value_img");
+            } catch (NullPointerException e){
+                Log.e("my_log", "error", e);}
+
+            String adr = intentItem.getStringExtra("string_value_adr");
+            String note = intentItem.getStringExtra("string_value_note");
+            String dist = intentItem.getStringExtra("string_value_dist");
+            String tel = intentItem.getStringExtra("string_value_tel");
+
+            AddItems ai = new AddItems(img, adr, note, dist, tel);
+            Log.e("1","new AddItems");
+
+            db.addAddItemsToSql(ai);
+            items.add(ai);
+
+            Log.e("1", "added new AddItems to SQL");
+
+            myAdapter.notifyDataSetChanged();
+        }
+    }
+
+
+}
